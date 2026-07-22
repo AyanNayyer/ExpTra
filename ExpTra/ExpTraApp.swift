03208c482f608f8d94cf45b64584069700898edc
Ayan Nayyer <ayannayyer@gmail.com>
2026-07-22 19:51:16 +0530

    Initial Commit

---
+++ ExpTra/ExpTraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpTraApp.swift
+//  ExpTra
+//
+//  Created by Ayan Nayyer on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpTraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
